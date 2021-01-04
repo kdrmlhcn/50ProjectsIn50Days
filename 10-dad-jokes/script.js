2b6d0271525f36ba81d21019f8c8b22043bbdb99
kdrmlhcn <kadirmelihcan@gmail.com>
2021-01-04 23:33:52 +0300

    add dad jokes

---
+++ 10-dad-jokes/script.js
@@ -0,0 +1,36 @@
+const jokeText = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// Using async / await
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeText.innerHTML = data.joke
+}
+
+// Using .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             jokeText.innerHTML = data.joke
+//         })
+// }
